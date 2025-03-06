f05ab20f0e67f7247a2dcd547a0b7921121458d1
bryanwessantana <bryanwessantana@gmail.com>
2025-03-05 21:39:37 -0300

    Activities 1 to 5 done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string number = Console.ReadLine();
+        int grade = int.Parse(number);
+
+        string letter = "";
+        string symbol = "";
+
+        //Message saying what is the User Grade
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >=80)
+        {
+            letter = "B"; 
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // STRETCH CHALLENGES 1 - 3
+
+        if (grade%10 >= 7)
+        {
+            symbol = "+";
+        }
+        else if (grade%10 < 3)
+        {
+            symbol = "-";
+        }
+        else
+        {
+            symbol = " ";
+        }
+
+        if (grade >= 93)
+        {
+            symbol = " ";
+        }
+        else if (grade <60)
+        {
+            symbol = " ";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{symbol}.");
+
+        // Message saying if he passed the class
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didn't passed the class. Better luck next time.");
+        }
     }
 }
