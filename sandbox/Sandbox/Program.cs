3b724b7c1a32bfcb173c2365df412bce76a11a7a
bryanwessantana <bryanwessantana@gmail.com>
2025-03-24 20:42:21 -0300

    Week 04 YouTubeVideos Program done

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,9 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        double a = 1.0;
-        decimal b = 2.1m;
-        Console.WriteLine(a + (double)b);
-        Console.WriteLine((decimal)a + b);
+        Console.WriteLine("Hello World! Welcome to the SandBox Program.");
     }
 }
